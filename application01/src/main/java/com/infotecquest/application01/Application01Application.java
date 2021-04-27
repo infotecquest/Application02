954ef41bc1d483a7efb2b97faae91327255144a6
Infotecquest <infotecquest@gmail.com>
2021-04-27 19:23:48 -0300

    Add files via upload

---
+++ application01/src/main/java/com/infotecquest/application01/Application01Application.java
@@ -0,0 +1,13 @@
+package com.infotecquest.application01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application01Application.class, args);
+	}
+
+}
---
+++ application01/src/test/java/com/infotecquest/application01/Application01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotecquest.application01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
